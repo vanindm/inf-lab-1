8bfb7759a8b498c7adfd0c3a9c0e7a6e94ea2fc8
dmvanin <dmvanin@edu.hse.ru>
2025-02-27 08:57:17 +0300

    MVP лабораторной работы

--- test.sh
+++ test.sh
@@ -7,11 +7,11 @@ TEMP_DIR=/tmp
 for OP in s p t l
 do
     for TYPE in i r c
-    do
+	do
         $EXEC -t $TYPE -o $OP -s < $TEST_DIR/$OP.$TYPE.in > $TEMP_DIR/$OP.$TYPE.out
         diff $TEST_DIR/$OP.$TYPE.out $TEMP_DIR/$OP.$TYPE.out &>/dev/null
         if [ $? -ne 0 ]; then
-            echo "Test for type $TYPE operation $OP failed!";
+            echo "Test for type $TYPE operation $OP failed!"
             exit 1
         fi
     done
